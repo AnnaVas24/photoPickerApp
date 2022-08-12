7719232633f4d7b965a5cec09ee97bbc271305c0
Anna <anna.vasi4ko@icloud.com>
2022-08-12 13:28:06 +0300

    Initial Commit

---
+++ photoPickerApp/photoPickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoPickerAppApp.swift
+//  photoPickerApp
+//
+//  Created by Vasichko Anna on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct photoPickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
